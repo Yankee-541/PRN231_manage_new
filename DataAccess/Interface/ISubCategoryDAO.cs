5bd8b70f814cba548b9379d7328a0654ec33a1f8
Vũ Đức Tiến <86123174+tiencoi2001@users.noreply.github.com>
2023-07-22 23:18:17 +0700

    [Add] SubCategory API

---
+++ DataAccess/Interface/ISubCategoryDAO.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogic.DTO;
+
+namespace DataAccess.Interface
+{
+    public interface ISubCategoryDAO
+    {
+        Task<List<SubCategoryDTO>> GetAllAsync();
+
+        Task<SubCategoryDTO> GetSubCategoryByIDAsync(int id);
+
+        Task CreateSubCategoryAsync(SubCategoryDTO subCategory);
+
+        Task UpdateSubCategoryAsync(SubCategoryDTO subCategory);
+
+        Task DeleteSubCategoryAsync(int id);
+    }
+}
--- Repositories/Interface/INewsBusiness.cs
+++ Repositories/Interface/INewsBusiness.cs
@@ -5,9 +5,10 @@ namespace Repositories.Interface
     public interface INewsBusiness
     {
         Task<List<NewsDTO>> SearchAsync(SearchModel searchModel);
+
         Task<List<NewsDTO>> GetListNews();
 
-		Task<NewsDTO> GetByIdAsync(int id);
+        Task<NewsDTO> GetByIdAsync(int id);
 
         Task<int> CreateAsync(NewsDTO dto);
 

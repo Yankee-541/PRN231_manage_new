4f6f0c4b8fddbb72fc7c4ff75cb2f8b9e65b3644
Vũ Đức Tiến <86123174+tiencoi2001@users.noreply.github.com>
2023-07-25 21:19:59 +0700

    Update

--- ManageNewsApi/Controllers/CommentController.cs
+++ ManageNewsApi/Controllers/CommentController.cs
@@ -1,38 +1,33 @@
 ﻿using BusinessLogic.DTO;
-using BusinessLogic.Models;
 using Microsoft.AspNetCore.Mvc;
-using Repositories.Business;
 using Repositories.Interface;
 
 namespace ManageNewsApi.Controllers
 {
-	[Route("api/[controller]/[action]")]
-	[ApiController]
-	public class CommentController : ControllerBase
-	{
-		private readonly ICommentBusiness _cmtBusiness;
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentBusiness _cmtBusiness;
 
+        public CommentController(ICommentBusiness newsBusiness)
+        {
+            _cmtBusiness = newsBusiness;
+        }
 
-		public CommentController(ICommentBusiness newsBusiness)
-		{
-			_cmtBusiness = newsBusiness;
-		}
+        [HttpPost]
+        public async Task CreateAsync([FromBody] CommenDTO dto)
+        {
+            var userId = Int32.Parse(User.FindFirst("Id")?.Value);
+            dto.CreatedBy = userId;
+            await _cmtBusiness.CreateCmt(dto);
+        }
 
-		[HttpPost]
-		public async Task CreateAsync([FromBody] CommenDTO dto)
-		{
-			var userId = Int32.Parse(User.FindFirst("Id")?.Value);
-			dto.CreatedBy = userId;
-			await _cmtBusiness.CreateCmt(dto);
-			
-		}
-
-		[HttpGet]
-		public async Task<IActionResult> GetCmtByNewIdAsync(int newId)
-		{
-			var users = await _cmtBusiness.GetCmtByNewId(newId);
-			return users != null ? Ok(users) : NotFound();
-		}
-
-	}
+        [HttpGet]
+        public async Task<IActionResult> GetCmtByNewIdAsync(int newId)
+        {
+            var cmt = await _cmtBusiness.GetCmtByNewId(newId);
+            return cmt != null ? Ok(cmt) : NotFound();
+        }
+    }
 }
--- Repositories/Interface/ICommentBusiness.cs
+++ Repositories/Interface/ICommentBusiness.cs
@@ -1,16 +1,11 @@
 ﻿using BusinessLogic.DTO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositories.Interface
 {
-	public interface ICommentBusiness
-	{
-		Task<int> CreateCmt(CommenDTO dto);
-		Task<CommenDTO> GetCmtByNewId(int newId);
+    public interface ICommentBusiness
+    {
+        Task<int> CreateCmt(CommenDTO dto);
 
-	}
+        Task<List<CommenDTO>> GetCmtByNewId(int newId);
+    }
 }
